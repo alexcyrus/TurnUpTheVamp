b80ac1c16bdc2fc103aa4468aa33801f9a5dc14a
Alex Cyrus <alex@alexcyrus.com>
2016-06-14 21:33:32 -0400

    getting enemies to show up on screen

--- public/js/game/states/Game.js
+++ public/js/game/states/Game.js
@@ -1,8 +1,12 @@
 ZenvaRunner.Game = function() {
 	this.playerMinAngle = -20;
 	this.playerMaxAngle = 20;
+
 	this.coinRate = 1000;
 	this.coinTimer = 0;
+
+	this.enemyRate = 500;
+	this.enemyTimer = 0;
 };
 
 ZenvaRunner.Game.prototype = {
@@ -35,6 +39,7 @@ ZenvaRunner.Game.prototype = {
 		this.player.body.bounce.set(0.25);
 
 		this.coins = this.game.add.group();
+		this.enemies = this.game.add.group();
 	},
 	update: function() {
 		if(this.game.input.activePointer.isDown) {
@@ -59,6 +64,11 @@ ZenvaRunner.Game.prototype = {
 			this.coinTimer = this.game.time.now + this.coinRate;
 		}
 
+		if(this.enemyTimer < this.game.time.now) {
+			this.createEnemy();
+			this.enemyTimer = this.game.time.now + this.enemyRate;
+		}
+
 		this.game.physics.arcade.collide(this.player, this.ground, this.groundHit, null, this);
 	},
 	shutdown: function() {
@@ -76,6 +86,19 @@ ZenvaRunner.Game.prototype = {
 
 		coin.reset(x, y);
 		coin.revive();
+	},
+	createEnemy: function() {
+		var x = this.game.width;
+		var y = this.game.rnd.integerInRange(50, this.game.world.height - 192);
+
+		var enemy = this.enemies.getFirstExists(false);
+		if(!enemy) {
+			enemy = new Enemy(this.game, 0, 0);
+			this.enemies.add(enemy);
+		}
+
+		enemy.reset(x, y);
+		enemy.revive();
 	},
 	groundHit: function(player, ground) {
 		player.body.velocity.y = -200;
